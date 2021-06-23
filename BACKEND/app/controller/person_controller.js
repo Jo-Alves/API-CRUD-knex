096e382fb1f5d594f8cadbad338bb1513f22065a
jo-alves <joelmirogerio@gmail.com>
2021-06-22 21:33:50 -0300

    first commit

---
+++ BACKEND/app/controller/person_controller.js
@@ -0,0 +1,55 @@
+const db = require("../../config/dbConnection");
+
+const personController = {
+    find: async (req, res) => {
+        try {
+            const persons = await (db.select().from("persons"))
+            res.status(200).send(persons);
+        } catch (error) {
+            console.error(error);
+        }
+    },
+    save: async (req, res) => {
+        const { name, cpf, address } = req.body;
+        try {
+            persons = {
+                name,
+                cpf,
+                address
+            }
+
+            if (!req.params.id) {
+                await db.insert(persons).into("persons");
+                res.status(200).send("Cadastro realizado com sucesso")
+            }
+            else {
+                if (isNaN(req.params.id)) {
+                    res.status(501).send("passe uma parâmentro válido");
+                    return
+                }
+                await db.update({
+                    name,
+                    address
+                }).table("persons").where({ id: parseInt(req.params.id) })
+                res.status(204).send("Atualizado com sucesso");
+            }
+        } catch (error) {
+            res.status(501).send({ error });
+        }
+    },
+    delete: async (req, res) => {
+        if (isNaN(req.params.id)) {
+            res.status(501).send("passe uma parâmentro válido");
+            return
+        }
+
+        try {
+            await db.delete().table("persons").where({ id: req.params.id })
+            res.status(204).send("excluido com sucesso")
+        } catch (error) {
+            res.status(501).send({ error });
+        }
+    }
+}
+
+module.exports = personController
---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <ListPersons msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import ListPersons from "./components/ListPersons.vue";
+
+export default {
+  name: "App",
+  components: {
+    ListPersons,
+  },
+};
+</script>
+
+<style>
+body{
+  margin: 0;
+  font-family: sans-serif;
+  padding: 0;
+  box-sizing: border-box;
+  font-size: 20px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
